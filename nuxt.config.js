67aa470d1885cba22a6843409232d4e0ceb16c99
NectoJ <nectoj@gmail.com>
2020-10-24 19:03:20 +0200

    Forgot to make target: static

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,9 @@ export default {
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
+  // For npm run generate
+  target: 'static',
+
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
